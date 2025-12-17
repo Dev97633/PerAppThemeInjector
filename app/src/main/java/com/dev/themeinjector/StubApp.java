75aa017fde84aafb2099499657c57a03e2a51c07
DEV <devendar87502@gmail.com>
2025-12-17 17:08:48 +0530

    Create StubApp.java

---
+++ app/src/main/java/com/dev/themeinjector/StubApp.java
@@ -0,0 +1,10 @@
+package com.dev.themeinjector;
+
+import android.app.Application;
+
+public class StubApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
